71b9c29acb65f58bada01698c5f7352173cea4d0
Hayri Aral <hayriaral@gmail.com>
2021-02-20 21:21:58 +0300

    Field injection

--- app/src/main/java/com/aralapps/android/study/disample/CarComponent.kt
+++ app/src/main/java/com/aralapps/android/study/disample/CarComponent.kt
@@ -4,6 +4,5 @@ import dagger.Component
 
 @Component
 interface CarComponent {
-
-    fun getCar(): Car
+    fun inject(mainActivity: MainActivity)
 }
--- app/src/main/java/com/aralapps/android/study/disample/MainActivity.kt
+++ app/src/main/java/com/aralapps/android/study/disample/MainActivity.kt
@@ -2,17 +2,19 @@ package com.aralapps.android.study.disample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var car: Car
+    @Inject
+    lateinit var car: Car
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val component: CarComponent = DaggerCarComponent.create()
-        car = component.getCar()
+        component.inject(this)
         car.drive()
     }
 }
